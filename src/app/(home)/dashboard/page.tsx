f65ddaad50b1ebfb57c94a739ea7b8de544d164f
johnmarkgatche <johngatchemark@gmail.com>
2025-05-05 14:21:41 +0800

    added dashboard cards

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,4 +1,7 @@
-import { Stack, Title } from "@mantine/core";
+import { Button, Group, Image, Stack, Title } from "@mantine/core";
+import brain from "@/app/_assets/brain.svg";
+import virus from "@/app/_assets/virus.svg";
+import lock from "@/app/_assets/lock.svg";
 
 export default function Dashboard() {
   return (
@@ -6,6 +9,81 @@ export default function Dashboard() {
       <Title c="cyan" order={1}>
         Activities
       </Title>
+
+      <Group>
+        <Stack
+          p="lg"
+          align="center"
+          w={300}
+          style={{
+            borderRadius: "15px",
+            background: "linear-gradient(180deg, #8859FF 0%, #4800FF 100%)",
+            aspectRatio: "1/1",
+          }}
+        >
+          <Title c="white" order={3}>
+            Password Strength
+          </Title>
+          <Image w={200} src={brain.src} />
+          <Button
+            style={{ alignSelf: "flex-end" }}
+            radius="xl"
+            variant="white"
+            c="#49299A"
+          >
+            Try Now
+          </Button>
+        </Stack>
+
+        <Stack
+          p="lg"
+          align="center"
+          w={300}
+          style={{
+            borderRadius: "15px",
+            background: "linear-gradient(180deg, #45EDF2 0%, #C0FDFF 100%)",
+            aspectRatio: "1/1",
+          }}
+        >
+          <Title c="#49299A" order={3}>
+            Phishing Simulator
+          </Title>
+          <Image w={200} src={virus.src} />
+          <Button
+            style={{ alignSelf: "flex-end" }}
+            radius="xl"
+            variant="filled"
+            bg="cyan.7"
+            c="white"
+          >
+            Try Now
+          </Button>
+        </Stack>
+
+        <Stack
+          p="lg"
+          align="center"
+          w={300}
+          style={{
+            borderRadius: "15px",
+            background: "linear-gradient(180deg, #E8E8FC 0%, #6400D5 100%)",
+            aspectRatio: "1/1",
+          }}
+        >
+          <Title c="#49299A" order={3}>
+            Drag Me, Drop Me
+          </Title>
+          <Image w={200} src={lock.src} />
+          <Button
+            style={{ alignSelf: "flex-end" }}
+            radius="xl"
+            variant="white"
+            c="#49299A"
+          >
+            Try Now
+          </Button>
+        </Stack>
+      </Group>
     </Stack>
   );
 }
